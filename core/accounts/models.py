114eccc64a3fe96258cfcfde69ba51211e3a8350
DevPrinceK <princesamuelpks@gmail.com>
2022-06-21 10:14:09 +0000

    create user model

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    index_number = models.CharField(max_length=12, unique=True)
+    fullname = models.CharField(max_length=60)
+    form = models.CharField(max_length=20)
+    user_class = models.CharField(max_length=20)
+    is_student = models.BooleanField(default=False)
+    is_done_voting = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.index_number
