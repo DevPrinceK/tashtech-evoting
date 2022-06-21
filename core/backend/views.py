d52b7972149b9df0b8c81fe876b606e6d80d5a33
DevPrinceK <princesamuelpks@gmail.com>
2022-06-21 09:52:28 +0000

    implement cbv for index page

--- core/backend/views.py
+++ core/backend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    template = 'backend/index.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template, {})
