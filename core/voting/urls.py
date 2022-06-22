963cca94896ec2da057231f0ffd480223b90f59c
DevPrinceK <princesamuelpks@gmail.com>
2022-06-22 11:48:23 +0000

    implement urls for voting app

--- core/voting/urls.py
+++ core/voting/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 app_name = 'voting'
 urlpatterns = [
-    path('', views.LandinPageView.as_view(), name='landing_page'),
+    path('vote/sp/', views.VoteSPView.as_view(), name='vote_sp'),
+    path('vote/gp/', views.VoteGPView.as_view(), name='vote_gp'),
+    path('vote/co/', views.VoteCOView.as_view(), name='vote_co'),
+    path('vote/lp/', views.VoteLPView.as_view(), name='vote_lp'),
 ]
