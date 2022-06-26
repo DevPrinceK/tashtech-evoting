6333d02312ea39fa289da2f2522e9216f2d92510
DevPrinceK <princesamuelpks@gmail.com>
2022-06-26 22:15:19 +0000

    Update dashboard.py

--- core/backend/views/dashboard.py
+++ core/backend/views/dashboard.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views import View
 
-from backend.models import Election
+from backend.models import Candidate, Election, Position
+from core.utils.constants import PortfolioName
 
 
 class IndexView(View):
@@ -9,5 +10,12 @@ class IndexView(View):
 
     def get(self, request, *args, **kwargs):
         current_election = Election.objects.filter(is_active=True).order_by('created_at').first()  # noqa
-        context = {'current_election': current_election}
+        positions = Position.objects.all().order_by('-id')
+        src_presidents = Candidate.objects.filter(position__name=PortfolioName.SP.value).order_by('ballot_number')  # noqa
+
+        context = {
+            'current_election': current_election,
+            'positions': positions,
+            'src_presidents': src_presidents,
+        }
         return render(request, self.template, context)
