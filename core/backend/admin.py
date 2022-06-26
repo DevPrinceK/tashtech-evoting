fe009117356398a50e3aaa0ef19c05656f17a258
DevPrinceK <princesamuelpks@gmail.com>
2022-06-26 17:05:49 +0000

    Update admin.py

--- core/backend/admin.py
+++ core/backend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Candidate)
+admin.site.register(Position)
+admin.site.register(Election)
