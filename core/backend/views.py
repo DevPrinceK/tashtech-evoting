73ab5954789f08409e9504b16a45c1d811e2ce40
DevPrinceK <princesamuelpks@gmail.com>
2022-06-26 17:09:04 +0000

    Delete views.py

--- core/backend/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-
-class IndexView(View):
-    template = 'backend/index.html'
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template, {})
