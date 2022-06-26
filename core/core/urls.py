aa66d90bc6aa8a4fa20adaf1ed5f91bf15540a3a
DevPrinceK <princesamuelpks@gmail.com>
2022-06-26 17:10:20 +0000

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,10 @@ urlpatterns = [
     path('backend/', include('backend.urls')),
     path('vote/', include('voting.urls')),
 ]
+
+# Let django serve static files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
