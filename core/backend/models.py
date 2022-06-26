1d1e7389d0d1764574f60307dba33e23e76f7672
DevPrinceK <princesamuelpks@gmail.com>
2022-06-26 17:06:46 +0000

    Update models.py

--- core/backend/models.py
+++ core/backend/models.py
@@ -1,3 +1,4 @@
+from turtle import position
 from django.db import models
 from django.utils import timezone
 
@@ -6,16 +7,9 @@ class Election(models.Model):
     '''Models for creating elections'''
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
-    date_of_election = models.DateTimeField(default=timezone.now)
+    date_of_election = models.DateTimeField(default=timezone.now)  # noqa
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Portfolio(models.Model):
-    '''Model to create and manage portfolios'''
-    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -25,10 +19,30 @@ class Candidate(models.Model):
     '''Model to create and manage cnadidates'''
     name = models.CharField(max_length=100)
     nickname = models.CharField(max_length=100, null=True, blank=True)
-    portfolio = models.ForeignKey(Portfolio, on_delete=models.SET_NULL, null=True, blank=True)
+    position = models.ForeignKey(
+        'Position', on_delete=models.SET_NULL, null=True, blank=True)
     picture = models.ImageField(upload_to='pictures', null=True, blank=True)
     ballot_number = models.IntegerField(default=1)
     vote_count = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    '''Model to create and manage positions'''
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_candidates(self):
+        '''Returns the number of candidates for the given position'''
+        return Candidate.objects.filter(position=self).count()
+
+    def get_votes_cast(self):
+        '''Returns the total votes cast for this position'''
+        cadidates = Candidate.objects.filter(position=self)
+        return sum([candidate.vote_count for candidate in cadidates])
 
     def __str__(self):
         return self.name
