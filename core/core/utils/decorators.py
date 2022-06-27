8e7a98d7a99e66f106e280d12bea906a30b741a9
DevPrinceK <princesamuelpks@gmail.com>
2022-06-27 11:17:28 +0000

    Update decorators.py

--- core/core/utils/decorators.py
+++ core/core/utils/decorators.py
@@ -10,15 +10,16 @@ class AdminOnly(object):
 
     def __call__(self, request, *args,  **kwargs):
         if request.user.is_authenticated:
-            if (request.user.is_superuser or request.user.is_staff):
+            if request.user.is_student:
+                # messages.error(request, 'Unauthorised Access is Prohibited!')  # noqa
+                return redirect('accounts:login')
+            elif (request.user.is_superuser or request.user.is_staff):
                 return self.original_method(request, *args, **kwargs)
             else:
-                messages.error(
-                    request, 'Unauthorised Access is Prohibited!')
+                # messages.error(request, 'Unauthorised Access is Prohibited!')  # noqa
                 return redirect('accounts:login')
         else:
-            messages.error(
-                request, 'Sorry, You must login to continue. ')
+            # messages.error(request, 'Sorry, You must login to continue. ')  # noqa
             return redirect('accounts:login')
 
 
@@ -33,10 +34,8 @@ class MustLogin(object):
             if (request.user.is_superuser or request.user.is_staff or request.user.is_student):  # noqa
                 return self.original_method(request, *args, **kwargs)
             else:
-                messages.error(
-                    request, 'Login Required!')
+                messages.error(request, 'Login Required!')  # noqa
                 return redirect('accounts:login')
         else:
-            messages.error(
-                request, 'Sorry, You must login to continue!')
+            messages.error(request, 'Sorry, You must login to continue!')  # noqa
             return redirect('accounts:login')
