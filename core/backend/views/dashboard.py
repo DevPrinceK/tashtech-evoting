4d17056a3bdac01df9d55ec4a26d2f3078a24e05
DevPrinceK <princesamuelpks@gmail.com>
2022-06-27 11:16:41 +0000

    Update dashboard.py

--- core/backend/views/dashboard.py
+++ core/backend/views/dashboard.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.views import View
 
 from backend.models import Candidate, Election, Position
+from django.utils.decorators import method_decorator
 from core.utils.constants import PortfolioName
+from core.utils.decorators import AdminOnly
 
 
 class IndexView(View):
     template = 'backend/index.html'
 
+    @method_decorator(AdminOnly)
     def get(self, request, *args, **kwargs):
         current_election = Election.objects.filter(is_active=True).order_by('created_at').first()  # noqa
         positions = Position.objects.all().order_by('-id')
