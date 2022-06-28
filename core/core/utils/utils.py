dab38ae5b7688e4bf5fd4ff58de6666841d68f39
DevPrinceK <princesamuelpks@gmail.com>
2022-06-28 11:10:42 +0000

    create util function to check if voter has already voted

--- core/core/utils/utils.py
+++ core/core/utils/utils.py
@@ -7,9 +7,3 @@ def check_already_voted(request):
         return redirect('voting:already_voted')
     pass
 
-
-def check_already_voted_sp(request):
-    '''Checks if voter has already voted for senior prefects'''
-    if request.user.voted_for_sp:
-        return redirect('voting:already_voted')
-    pass
