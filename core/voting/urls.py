c17e5ae212fd5d3cbc234de413e1db1432a3afcd
DevPrinceK <princesamuelpks@gmail.com>
2022-06-28 09:10:36 +0000

    Update urls.py

--- core/voting/urls.py
+++ core/voting/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'voting'
 urlpatterns = [
-    path('start/', views.StartVotingView.as_view(), name='start_voting'),
+    path('instructions/', views.InstructionsView.as_view(), name='instructions'),
+    path('vote-completed/', views.VoteCompletedView.as_view(), name='vote_completed'),  # noqa
+    path('already-voted/', views.AlreadyVotedView.as_view(), name='already_voted'),  # noqa
     path('sp/', views.VoteSPView.as_view(), name='vote_sp'),
     path('gp/', views.VoteGPView.as_view(), name='vote_gp'),
     path('co/', views.VoteCOView.as_view(), name='vote_co'),
