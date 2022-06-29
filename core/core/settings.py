57836cbd2795d33bbe612e55855e8c7468cfc5b8
DevPrinceK <princesamuelpks@gmail.com>
2022-06-29 04:21:56 +0000

    update

--- core/core/settings.py
+++ core/core/settings.py
@@ -106,8 +106,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles/'
+STATIC_ROOT = BASE_DIR / 'static/'
 MEDIA_ROOT = BASE_DIR / 'assets/'
 MEDIA_URL = '/assets/'
 
