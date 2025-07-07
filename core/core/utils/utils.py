f5c0a43cfe0435c7a8aaa78ca19420bdb72b629b
princek <princesamuelpks@gmail>
2025-07-07 08:44:38 +0000

    update

--- core/core/utils/utils.py
+++ core/core/utils/utils.py
@@ -3,20 +3,6 @@ from django.shortcuts import redirect
 from backend.models import Position
 
 
-def check_already_voted(user):
-    '''Checks if voter has already voted'''
-    if user.voted_for_all:
-        return redirect('voting:already_voted')
-
-
-def vote_for_all(request):
-    '''flags voters who have already voted for all positions'''
-    voter = request.user
-    # if voter.voted_for_sp and voter.voted_for_gp and voter.voted_for_cob and voter.voted_for_cog and voter.voted_for_sgpb and voter.voted_for_sgpg and voter.voted_for_dhpb and voter.voted_for_dhpg and voter.voted_for_ecpb and voter.voted_for_ecpg and voter.voted_for_lpb and voter.voted_for_lpg and voter.voted_for_csb and voter.voted_for_csg and voter.voted_for_hspb and voter.voted_for_hspg and voter.voted_for_hpb and voter.voted_for_hpg:
-    voter.voted_for_all = True
-    voter.save()
-
-
 def get_next_position(request) -> int:
     '''
     returns the next position that the voter is
--- core/voting/urls.py
+++ core/voting/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import voting_view as views
+from . import views as views
 
 
 app_name = 'voting'
@@ -7,66 +7,7 @@ app_name = 'voting'
 # generals
 urlpatterns = [
     path('instructions/', views.InstructionsView.as_view(), name='instructions'),
-    path('vote-completed/', views.VoteCompletedView.as_view(), name='vote_completed'),  # noqa
-    path('already-voted/', views.AlreadyVotedView.as_view(), name='already_voted'),  # noqa
+    path('castvote/', views.CastVoteView.as_view(), name='cast_vote'),  # noqa
+    path('votecompleted/', views.VoteCompletedView.as_view(), name='vote_completed'),  # noqa
+    path('alreadyvoted/', views.AlreadyVotedView.as_view(), name='already_voted'),  # noqa
 ]
-
-# # senior prefects
-# urlpatterns += [
-#     path('vote-sp/', views.VoteSPView.as_view(), name='vote_sp'),
-#     path('vote-gp/', views.VoteGPView.as_view(), name='vote_gp'),
-# ]
-
-# # compound overseers
-# urlpatterns += [
-#     path('vote-cob/', views.VoteCOBView.as_view(), name='vote_cob'),
-#     path('vote-cog/', views.VoteCOGView.as_view(), name='vote_cog'),
-# ]
-
-# # sports & games prefects
-# urlpatterns += [
-#     path('vote-sgpb/', views.VoteSGPBView.as_view(), name='vote_sgpb'),
-#     path('vote-sgpg/', views.VoteSGPGView.as_view(), name='vote_sgpg'),
-# ]
-
-# # dining hall prefects
-# urlpatterns += [
-#     path('vote-dhpb/', views.VoteDHPBView.as_view(), name='vote_dhpb'),
-#     path('vote-dhpg/', views.VoteDHPGView.as_view(), name='vote_dhpg'),
-# ]
-
-# # entertainment prefects
-# urlpatterns += [
-#     path('vote-ecpb/', views.VoteECPBView.as_view(), name='vote_ecpb'),
-#     path('vote-ecpg/', views.VoteECPGView.as_view(), name='vote_ecpg'),
-# ]
-
-# # library prefects
-# urlpatterns += [
-#     path('vote-lpb/', views.VoteLPBView.as_view(), name='vote_lpb'),
-#     path('vote-lpg/', views.VoteLPGView.as_view(), name='vote_lpg'),
-# ]
-
-# # chapel steward prefects
-# urlpatterns += [
-#     path('vote-csb/', views.VoteCSBView.as_view(), name='vote_csb'),
-#     path('vote-csg/', views.VoteCSGView.as_view(), name='vote_csg'),
-# ]
-
-# # health and sanitation prefects
-# urlpatterns += [
-#     path('vote-hspb/', views.VoteHSPBView.as_view(), name='vote_hspb'),
-#     path('vote-hspg/', views.VoteHSPGView.as_view(), name='vote_hspg'),
-# ]
-
-# # house prefects
-# urlpatterns += [
-#     path('vote-hpb/', views.VoteHPBView.as_view(), name='vote_hpb'),
-#     path('vote-hpg/', views.VoteHPGView.as_view(), name='vote_hpg'),
-# ]
-
-# # day students coordinator
-# urlpatterns += [
-#     path('vote-dscb/', views.VoteDSCBView.as_view(), name='vote_dscb'),
-#     path('vote-dscg/', views.VoteDSCGView.as_view(), name='vote_dscg'),
-# ]
